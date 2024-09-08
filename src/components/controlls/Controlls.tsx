26650f79cceed30d701a6a19ba5e0c22ba717f86
PromiSe#### <vicimpa@ya.ru>
2024-09-09 00:13:19 +0700

    Fix controlls

--- src/components/controlls/Controlls.tsx
+++ src/components/controlls/Controlls.tsx
@@ -1,13 +1,15 @@
-import { ChangeEvent, FC, useRef } from "react";
+import { ChangeEvent, FC, useRef, useState } from "react";
 
 import s from "./Controlls.module.sass";
 import { useFrames } from "$hooks/useFrames";
 import { usePlayerRef } from "$components/player";
 
 export const Controlls: FC = () => {
+  const container = useRef<HTMLDivElement>(null);
   const range = useRef<HTMLInputElement>(null);
   const button = useRef<HTMLButtonElement>(null);
   const video = usePlayerRef();
+  const [moved, setMoved] = useState(0);
 
   const playPause = () => {
     if (video.current) {
@@ -38,7 +40,11 @@ export const Controlls: FC = () => {
     }
   };
 
-  useFrames(() => {
+  const mouseEvent = () => {
+    setMoved(performance.now());
+  };
+
+  useFrames((_, time) => {
     if (!video.current || !range.current)
       return;
 
@@ -46,16 +52,42 @@ export const Controlls: FC = () => {
     range.current.max = `${video.current.duration}`;
     range.current.value = `${video.current.currentTime}`;
 
-    if (!button.current)
-      return;
+    if (container.current) {
+      const div = container.current;
+      const { classList } = div;
 
-    const text = video.current.paused ? '⏵' : '⏸';
-    if (button.current.innerText !== text)
-      button.current.innerText = text;
+      if (document.fullscreenElement?.contains(div)) {
+        classList.add(s.fullscreen);
+
+        if (time < moved + 1000 || video.current.paused) {
+          classList.add(s.moved);
+        } else {
+          classList.remove(s.moved);
+        }
+
+      } else {
+        classList.remove(s.fullscreen);
+      }
+    }
+
+    if (button.current) {
+      const text = video.current.paused ? '⏵' : '⏸';
+
+      if (button.current.innerText !== text)
+        button.current.innerText = text;
+    }
   });
 
   return (
-    <div className={s.controlls} onClick={playPause}>
+    <div
+      className={s.controlls}
+      ref={container}
+      onClick={playPause}
+      onMouseDown={mouseEvent}
+      onMouseMove={mouseEvent}
+      onKeyDown={mouseEvent}
+      onDoubleClick={toggleFullscreen}
+    >
       <div className={s.pannel}>
         <div style={{ display: 'contents' }} onClick={e => e.stopPropagation()}>
           <button ref={button} onClick={playPause}>
