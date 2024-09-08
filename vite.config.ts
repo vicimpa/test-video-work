99f93b73ec62f41fc0a0267294c611e762a53008
PromiSe#### <vicimpa@ya.ru>
2024-09-08 22:17:30 +0700

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   root: './src',
-  publicDir: '../',
+  publicDir: '../public',
   build: {
     emptyOutDir: true,
     outDir: '../dist',
