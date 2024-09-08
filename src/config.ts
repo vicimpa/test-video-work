0b06a43afc060c5ca7cf37e3115d5b755780bf1e
PromiSe#### <vicimpa@ya.ru>
2024-09-08 23:07:45 +0700

    Fix config

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const videoSrc = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
-export const eventsSrc = "https://5025y.wiremockapi.cloud/json/1";
+export const videoSrc = "//commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+export const eventsSrc = "//5025y.wiremockapi.cloud/json/1";
